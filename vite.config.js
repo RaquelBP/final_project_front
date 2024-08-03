b3346d18b499f47b3feb09cca8ce19a0c6d76a3e
Raquel Benito <raquelbenitop@gmail.com>
2024-08-03 22:22:53 +0200

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   define: {
     "process.env": {},
   },
